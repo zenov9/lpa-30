5624ffe12c443040101a1cea2ff823ad27b04705
Angelo <angelo.gabriiell2@gmail.com>
2023-03-17 17:03:49 -0300

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 class Simple {  
     public static void main(String args[]) {  
        System.out.println("Hello Java");
-       System.out.println("olá pessoa");     
-    }  
+       System.out.println("olá pessoa");
+       System.out.println("eu amo pizza");
+    }
  }  
